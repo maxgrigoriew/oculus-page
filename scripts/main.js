44f2a092c5a04fa99efd87bc7e20c8e096797145
Maxim Hlopyanik <sss123123111@yandex.ru>
2023-06-01 11:24:53 +0300

    add desktop

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+const tabItems = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+
+const open = (e) => {
+  const tabTarget = e.currentTarget
+  const button = tabTarget.dataset.button
+
+  tabItems.forEach((item, index) => {
+    item.classList.remove('tabs__btn-item--active')
+  })
+
+  tabContent.forEach((item, index) => {
+    item.classList.remove('tabs__content-item--active')
+  })
+  
+  tabTarget.classList.add('tabs__btn-item--active')
+  document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
+
+  
+}
+
+tabItems.forEach((item) => {
+  item.addEventListener("click", open);
+});
+
